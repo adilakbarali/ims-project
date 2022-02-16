711ae1b7d718a20745efb19cd7d45fc28275e29b
adilakbarali <adilakbarali@gmail.com>
2022-02-16 11:58:07 +0000

    Added Item, ItemDAO and ItemController. Modified IMS to incorporate the new classes. Updated db.properties with correct password for Database

---
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -0,0 +1,86 @@
+package com.qa.ims.persistence.domain;
+
+public class Item {
+
+	private Long id;
+	private String itemName;
+	private Double value;
+
+	public Item(String itemName, Double value) {
+		this.setItemName(itemName);
+		this.setValue(value);
+	}
+
+	public Item(Long id, String itemName, Double value) {
+		this.setId(id);
+		this.setItemName(itemName);
+		this.setValue(value);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "id:" + id + " item name:" + itemName + " value:£" + value;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (getItemName() == null) {
+			if (other.getItemName() != null)
+				return false;
+		} else if (!getItemName().equals(other.getItemName()))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+}
